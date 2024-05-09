277e600af0a98ac8395f305d5cf8ebfed6e07744
BonusDucksMkII <indifferential.equations@gmail.com>
2024-05-08 21:27:34 -0600

    Finished Learning Wk 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction One = new Fraction(1);
+        Fraction Five = new Fraction(5);
+        Fraction ThreeOverFour = new Fraction(3, 4);
+        Fraction OneOverThree = new Fraction(1, 3);
+
+        Console.WriteLine(One.GetFractionString());
+        Console.WriteLine($"{One.GetDecimalValue()}\n");
+
+        Console.WriteLine(Five.GetFractionString());
+        Console.WriteLine($"{Five.GetDecimalValue()}\n");
+
+        Console.WriteLine(ThreeOverFour.GetFractionString());
+        Console.WriteLine($"{ThreeOverFour.GetDecimalValue()}\n");
+
+        Console.WriteLine(OneOverThree.GetFractionString());
+        Console.WriteLine($"{OneOverThree.GetDecimalValue()}");
     }
 }
