fc2c645733e9bb0d973a94492d9ace79751ebabc
BonusDucksMkII <indifferential.equations@gmail.com>
2024-05-14 17:36:17 -0600

    Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment Sam = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(Sam.GetSummary());
+        MathAssignment Rob = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(Rob.GetSummary());
+        Console.WriteLine(Rob.GetHomeworkList());
+        WritingAssignment Mary = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(Mary.GetSummary());
+        Console.WriteLine(Mary.GetWritingInformation());
     }
 }
